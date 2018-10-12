539dbb08b551ab867e4dfda25f4963eb48fa51c1
Gavin <gavinyahna@brandeis.edu>
2018-10-12 15:07:17 -0400

    Add header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :events
   resources :notes
   resources :users
+  root 'application#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
