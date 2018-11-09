7b6fa22e5e8dbbfde8becbbb2383aee83b46d59b
Darhan Rzaev <Darhan@Darhans-MacBook-Pro.local>
2018-11-08 22:01:30 -0500

    modified

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-# OmniAuth.config.logger = Rails.logger
+ OmniAuth.config.logger = Rails.logger
 #
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :google_oauth2, '727516764491-jnin4n5pgethaohjnj8u4ujihpvt0uoo.apps.googleusercontent.com', 'eQjc_EQteEXgdM_bz9AUOz3W', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
@@ -13,6 +13,8 @@ evernote = {
 evernote_site = evernote[:sandbox] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '727516764491-jnin4n5pgethaohjnj8u4ujihpvt0uoo.apps.googleusercontent.com', 'eQjc_EQteEXgdM_bz9AUOz3W', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+
   provider :evernote,
            evernote[:consumer_key],
            evernote[:consumer_secret],
