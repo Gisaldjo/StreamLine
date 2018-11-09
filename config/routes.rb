5b8fe82a167ea06fba3670fe4cab918b320a496a
Gavin <gavinyahna@brandeis.edu>
2018-11-09 00:54:18 -0500

    create tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 StreamLine::Application.routes.draw do
+  resources :tasks
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
