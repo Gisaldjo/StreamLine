49112f359230c71cd24b06568a3fa387a6197a4f
Gavin <gavinyahna@brandeis.edu>
2018-11-09 02:50:09 -0500

    fix adding notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -25,16 +25,7 @@ class NotesController < ApplicationController
   # POST /notes.json
   def create
     @note = Note.new(note_params)
-
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
-    end
+    @note.save
   end
 
   # PATCH/PUT /notes/1
@@ -69,6 +60,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:title, :description, :group)
+      params.permit(:title, :description, :group)
     end
 end
