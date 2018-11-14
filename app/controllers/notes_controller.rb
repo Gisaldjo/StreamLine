8d70106ab6ac084706b6bf2cb0b87f6a72fa806b
Gavin <gavinyahna@brandeis.edu>
2018-11-14 01:08:20 -0500

    make notes blue and fix updating

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -63,6 +63,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.permit(:title, :description, :group)
+      params.require(:note).permit(:title, :description, :group)
     end
 end
