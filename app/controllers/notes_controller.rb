7b46209a4eb35a1403f8351d174cb37204d15d4f
Gavin <gavinyahna@brandeis.edu>
2018-11-14 00:54:40 -0500

    change style of note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,7 +10,6 @@ class NotesController < ApplicationController
   # GET /notes/1
   # GET /notes/1.json
   def show
-
     respond_to do |format|
       format.js { @currNote = params[:id] }
     end
@@ -39,11 +38,8 @@ class NotesController < ApplicationController
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+        format.html { redirect_to :root, notice: 'Note was successfully updated.' }
         format.json { render :show, status: :ok, location: @note }
-      else
-        format.html { render :edit }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,10 +47,11 @@ class NotesController < ApplicationController
   # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
-    @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
-      format.json { head :no_content }
+      if @note.destroy
+        format.html { redirect_to :root, notice: 'Note was successfully updated.' }
+        format.json { render :show, status: :ok, location: @note }
+      end
     end
   end
 
